fde6f921dfb916a1613556ff09a241a677367edf
ailtontelesxavier <atxshared@gmail.com>
2023-08-17 08:34:33 -0300

    alteracao

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,7 +1,9 @@
+import 'package:app_repeat/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({super.key});
+  final Category category;
+  const CategoriesMealsScreen(this.category, {super.key});
 
   @override
   Widget build(BuildContext context) {
