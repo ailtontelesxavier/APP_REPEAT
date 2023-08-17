d92e89fddb9900fa319fb3725008b349f5df3dd8
ailtontelesxavier <atxshared@gmail.com>
2023-08-17 10:42:25 -0300

    componente item refeicao 02

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,20 +1,28 @@
-import 'package:app_repeat/models/category.dart';
+import 'package:app_repeat/components/meal_item.dart';
 import 'package:flutter/material.dart';
+import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  //final Category category;
-  //const CategoriesMealsScreen(this.category, {super.key});
-  const CategoriesMealsScreen({super.key});
+  const CategoriesMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
